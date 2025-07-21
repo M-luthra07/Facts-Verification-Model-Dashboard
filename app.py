b53b36fd0593eee99d91691f86d64ab0070fd39e
Manya Luthra <manya22csu458@ncuindia.edu>
2025-07-21 15:44:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ def predict():
                                prediction=label,
                                confidence=round(confidence, 2))
 
-
+port = int(os.environ.get("PORT", 10000))
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)

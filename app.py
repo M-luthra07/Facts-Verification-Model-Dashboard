faaa580a730196ad4483128d8f79f8cb8f897177
Manya Luthra <manya22csu458@ncuindia.edu>
2025-07-21 15:29:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import re
 import numpy as np
 import tensorflow as tf
+import os
 from transformers import BertTokenizer
 
 app = Flask(__name__)
@@ -50,4 +51,5 @@ def predict():
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
+   port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
